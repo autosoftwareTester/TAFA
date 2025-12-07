6b88d83329cca209dd151edf6b5f3b0387ab84fc
igwe <autotestsoftware@gmail.com>
2025-12-07 14:45:38 +0200

    add workflow

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f26c7491c1d933a9e131e8ceaa1d0c3ed93be5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3415951bb9d7bb72747ebde827ace22a1cda092f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
